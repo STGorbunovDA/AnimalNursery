9f69b906e5c133da2cd2cb51494a33168ae386e7
STGorbunovDA <bous07@mail.ru>
2023-10-05 01:41:15 +0900

    Добавлен базовый класс животного

---
+++ AnimalNursery/Models/Base/Animal.cs
@@ -0,0 +1,42 @@
+﻿using AnimalNursery.Models.Base.Interfaces;
+using AnimalNursery.ViewModels.Base;
+
+namespace AnimalNursery.Models.Base
+{
+    public class Animal : ViewModelBase, IExecuteCommand
+    {
+        private int _id;
+        private string _name;
+        private int _age;
+        private double _height;
+        private double _weight;
+        private string _classAnimal;
+        private string _command;
+
+        public Animal(int id, string name, int age, double height, double weight, string classAnimal, string command)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            ClassAnimal = classAnimal;
+            Command = command;
+        }
+
+        public int Id { get => _id; set { _id = value; OnPropertyChanged(nameof(Id)); } }
+        public string Name { get => _name; set { _name = value; OnPropertyChanged(nameof(Name)); } }
+        public int Age { get => _age; set { _age = value; OnPropertyChanged(nameof(Age)); } }
+        public double Height { get => _height; set { _height = value; OnPropertyChanged(nameof(Height)); } }
+        public double Weight { get => _weight; set { _weight = value; OnPropertyChanged(nameof(Weight)); } }
+        public string ClassAnimal { get => _classAnimal; set { _classAnimal = value; OnPropertyChanged(nameof(ClassAnimal)); } }
+        public string Command { get => _command; set { _command = value; OnPropertyChanged(nameof(Command)); } }
+
+
+
+        public string ExecuteCommand()
+        {
+            return Command;
+        }
+    }
+}
